16016662a3b5e57ea7768706e8027a959dcce1b9
Carmenyo <carmencastroyo@gmail.com>
2023-09-21 18:11:53 +0200

    JS validation form working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log ("Hola");
+"use strict";
+
+const inputEmail = document.querySelector (".js-input");
+const submitButton = document.querySelector (".js-button");
+const errorMessage = document.querySelector (".js-error");
+
+submitButton.addEventListener ("click", (e) =>{
+    e.preventDefault();
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (inputEmail.value.match(re)){
+        console.log ("valid")
+    }
+    else{
+        console.log ("error")
+
+    }
+});
+
