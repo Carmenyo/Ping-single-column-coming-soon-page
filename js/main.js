8ad16f02e28633a35938f3c5d7a936ecf3082b5f
Carmenyo <carmencastroyo@gmail.com>
2023-09-22 16:23:32 +0200

    fxing layout pixel perfect

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const error = document.getElementById("errorText");
 const submitButton = document.getElementById("button");
 const success = document.getElementById("successText");
 
-
 // FUNCION DEL BOTON
 
 submitButton.addEventListener("click", (e) => {
@@ -18,11 +17,13 @@ submitButton.addEventListener("click", (e) => {
     errorText.style.display = "none";
     successText.style.display = "block";
     // PONER EL VALOR A 0
-    email.value = " "
+    email.value = " ";
 
     email.classList.remove("error");
   } else {
     errorText.style.display = "block";
     email.classList.add("error");
+    // CAMBIAR EL VALOR DE INPUT
+    email.value = "example@email.com";
   }
 });
