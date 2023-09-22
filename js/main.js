2fd9399ad789ac579ae1f0de566726cb6a7b0371
Carmenyo <carmencastroyo@gmail.com>
2023-09-22 16:11:46 +0200

    JS send succesfully and clear the input

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 "use strict";
-
+// DECLARACION DE CONSTANTES
 const email = document.getElementById("emailInput");
 const error = document.getElementById("errorText");
 const submitButton = document.getElementById("button");
+const success = document.getElementById("successText");
 
 
+// FUNCION DEL BOTON
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
+
+  // FUNCION DE COMPROBACIÖN
   const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
   if (email.value.match(re)) {
     errorText.style.display = "none";
-    emailInput.classList.remove("error");
-   
+    successText.style.display = "block";
+    // PONER EL VALOR A 0
+    email.value = " "
+
+    email.classList.remove("error");
   } else {
     errorText.style.display = "block";
-    emailInput.classList.add("error");
+    email.classList.add("error");
   }
 });
-
-
