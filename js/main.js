345346f696102f98ed1140d8774909296a1ea68c
Carmenyo <carmencastroyo@gmail.com>
2023-09-22 09:47:26 +0200

    JS error message working

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 "use strict";
 
-const inputEmail = document.querySelector (".js-input");
-const submitButton = document.querySelector (".js-button");
-const errorMessage = document.querySelector (".js-error");
+const email = document.getElementById("emailInput");
+const error = document.getElementById("errorText");
+const submitButton = document.getElementById("button");
 
-submitButton.addEventListener ("click", (e) =>{
-    e.preventDefault();
-    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    if (inputEmail.value.match(re)){
-        console.log ("valid")
-    }
-    else{
-        console.log ("error")
 
-    }
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (email.value.match(re)) {
+    errorText.style.display = "none";
+   
+  } else {
+    errorText.style.display = "block";
+   
+  }
 });
+
 
